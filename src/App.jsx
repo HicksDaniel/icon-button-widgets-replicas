8406af6e8f3179dde21c6a9ab12936156983aa69
Daniel Hicks <danielehicks89@gmail.com>
2025-03-12 13:00:56 -0400

    Creating Rotatable Display with scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import "./App.css";
-import FetchMovies from "./replicas/MovieDisplay/FetchMovies";
 
-import MovieShowcase from "./replicas/MovieDisplay/MovieShowcase";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import MediaCard from "./MediaCard";
 
 function App() {
   return (
     <>
-      <MovieShowcase />
-      <FetchMovies />
+      <div className="phone-container">
+        <div className="phone">
+          <div className="black-bar"></div>
+          <div className="phone-contents">
+            <div className="contents-layout">
+              <h1>Title</h1>
+
+              <MediaCard></MediaCard>
+
+              <MediaCard />
+
+              <MediaCard></MediaCard>
+              <MediaCard></MediaCard>
+              <MediaCard></MediaCard>
+
+              <MediaCard></MediaCard>
+            </div>
+          </div>
+        </div>
+        <div className="phone-shadow">shadow</div>
+      </div>
     </>
   );
 }
